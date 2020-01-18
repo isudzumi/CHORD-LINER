6547a7f43c8ce251a3d7c517d017af688f60453e
isudzumi <isudzumi@gmail.com>
2020-01-18 19:10:57 +0900

    add: install chalice

---
+++ CHORD-LINER/CHORD_LINER.py
@@ -0,0 +1,8 @@
+﻿from chalice import Chalice
+
+app = Chalice(app_name="chord-liner")
+
+@app.route('/', methods=['POST'])
+def index():
+    request = app.current_request
+    print(request.json_body)
