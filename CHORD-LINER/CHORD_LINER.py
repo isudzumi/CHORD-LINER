61ecc86675f70f0b182903ebf8097a8d98c470e7
isudzumi <isudzumi@gmail.com>
2020-01-19 17:32:02 +0900

    add: change directory structure and chalice config

--- CHORD-LINER/CHORD_LINER.py
+++
@@ -1,8 +0,0 @@
-﻿from chalice import Chalice
-
-app = Chalice(app_name="chord-liner")
-
-@app.route('/', methods=['POST'])
-def index():
-    request = app.current_request
-    print(request.json_body)
