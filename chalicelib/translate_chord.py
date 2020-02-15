be9087e5adb0c721ff5e9fd2b522c9c049aed9d0
isudzumi <isudzumi@gmail.com>
2020-02-15 14:38:07 +0900

    Update translate chord

--- chalicelib/translate_chord.py
+++ chalicelib/translate_chord.py
@@ -1,12 +1,14 @@
 from pychord import Chord
 import numpy as np
+from scipy.io import wavfile
 from functools import reduce
 import math
 from typing import List, Dict
 import itertools
 
-SECONDS = 3.0
+SECONDS = 2.0
 BASE_FREQUENCY = 440 # A=440Hz
+SAMPLING_RATE = 44100
 SCALE = [ 'A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#' ]
 
 def analyze_chord(symbol: str) -> List[str]:
@@ -30,15 +32,26 @@ def make_note_index_list(chords: List[str]) -> List[int]:
     note_indexes = [ SCALE.index(chord) for chord in chords ]
     return reduce(accumulate_note_order, note_indexes, [])
 
-def make_frequency_map(notes: List[str]) -> Dict[str, float]:
+def translate_to_freauencies(notes: List[str]) -> List[float]:
     note_index_list = make_note_index_list(notes)
-    frequency_list = make_frequency_list(note_index_list)
-    return { note_name: frequency for note_name, frequency in zip(notes, frequency_list) }
+    return make_frequency_list(note_index_list)
 
-def compose_note(notes: List[str]):
-    pass
+def create_sin_wave(frequency: float):
+    phases = np.cumsum(2.0 * np.pi * frequency / SAMPLING_RATE * np.ones(int(SAMPLING_RATE * SECONDS)))
+    return np.sin(phases)
+
+def compose_note(frequencies: List[float]):
+    translate = np.frompyfunc(create_sin_wave, 1, 1)
+    sin_waves = translate(frequencies)
+    return np.sum(sin_waves, axis=0)
+
+def translate_chord(chord_signal: str):
+    notes = analyze_chord(chord_signal)
+    frequency_list = translate_to_freauencies(notes)
+    return compose_note(frequency_list)
 
 if __name__ == '__main__':
-    notes = analyze_chord('G11')
-    frequency_map = make_frequency_map(notes)
-    print(frequency_map)
+    chord = translate_chord('Gadd9')
+    wave_signals = 0.1*(chord)
+    wave = (wave_signals * float(2 ** 15 - 1)).astype(np.int16)
+    wavfile.write('Dadd9.wav', SAMPLING_RATE, wave)
