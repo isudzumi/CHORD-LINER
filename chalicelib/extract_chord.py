ca72a90bb8eee7f84e796a710e6c678cedfc3d7b
isudzumi <isudzumi@gmail.com>
2020-02-29 22:28:59 +0900

    Allow line feed & Fix M7

--- chalicelib/extract_chord.py
+++ chalicelib/extract_chord.py
@@ -3,7 +3,7 @@ import re
 OPERATION_KEYWORD = '演奏して'
 
 def extract_chord(text: str) -> str:
-    result = re.match('^{}\n([A-G][a-z0-9]*)'.format(OPERATION_KEYWORD), text)
+    result = re.match('^{}\n?([A-G][Ma-z0-9]*)'.format(OPERATION_KEYWORD), text)
     return result.group(1) if result is not None else ''
 
 if __name__ == "__main__":
--- tests/test_extract_chord.py
+++ tests/test_extract_chord.py
@@ -3,6 +3,9 @@ from chalicelib.extract_chord import extract_chord
 def test_single_major_chord():
     assert extract_chord("演奏して\nC") == 'C'
 
+def test_if_allow_line_feed():
+    assert extract_chord('演奏してC') == 'C'
+
 def test_no_input():
     assert extract_chord('') == ''
 
@@ -14,4 +17,7 @@ def test_plural_chord():
 
 def test_ending_with_number():
     assert extract_chord('演奏して\nCadd9') == 'Cadd9'
+
+def test_major_seventh():
+    assert extract_chord('演奏して\nCM7') == 'CM7'
     
