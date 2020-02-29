5b2fdc6903c62153fafde6a2a54a8be5d5746156
isudzumi <isudzumi@gmail.com>
2020-02-29 18:43:25 +0900

    Update translate_chord

--- chalicelib/translate_chord.py
+++ chalicelib/translate_chord.py
@@ -3,7 +3,7 @@ import numpy as np
 from scipy.io import wavfile
 from functools import reduce
 import math
-from typing import List, Dict
+from typing import List
 import itertools
 
 SECONDS = 2.0
@@ -18,10 +18,10 @@ def analyze_chord(symbol: str) -> List[str]:
 def make_frequency_list(note_indexes: List[int]) -> List[float]:
     return [ BASE_FREQUENCY * 2 ** ( i / 12 ) for i in note_indexes ]
 
-def get_current_octave(interval: int):
+def get_current_octave(interval: int) -> int:
     return math.floor(interval / len(SCALE)) * len(SCALE)
 
-def raise_octave(order):
+def raise_octave(order: int) -> int:
     return order + len(SCALE)
 
 def accumulate_note_order(intervals: List[int], order: int) -> List[int]:
@@ -49,16 +49,16 @@ def compose_note(frequencies: List[float]):
     sin_waves = translate(frequencies)
     return np.sum(sin_waves, axis=0)
 
-def save_wavefile(chord_signal):
-    wave_signals = 0.1*(chord_signal)
-    wave = (wave_signals * float(2 ** 15 - 1)).astype(np.int16)
-    wavfile.write('Dadd9.wav', SAMPLING_RATE, wave)
-
 def translate_chord(chord_symbol: str):
     notes = analyze_chord(chord_symbol)
+    if not notes:
+        return {}
     frequency_list = translate_to_freauencies(notes)
-    return compose_note(frequency_list)
+    return {
+        'signals': compose_note(frequency_list),
+        'duration': SECONDS,
+        'sampling_rate': SAMPLING_RATE
+    }
 
 if __name__ == '__main__':
-    chord = translate_chord('Gadd9')
-    save_wavefile(chord)
+    pass
