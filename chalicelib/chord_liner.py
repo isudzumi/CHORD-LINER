17c0531fc98a282bde663a4dea40b28b94515ed6
isudzumi <isudzumi@gmail.com>
2020-02-29 18:44:12 +0900

    Add main function

---
+++ chalicelib/chord_liner.py
@@ -0,0 +1,23 @@
+from typing import Dict
+from extract_chord import extract_chord
+from translate_chord import translate_chord
+from save_file import save_file
+
+def get_chord(event):
+    chord = extract_chord(event.message.text)
+    if not chord:
+        return {}
+    audio = translate_chord(chord)
+    audio.update({
+        'file_name': '{}.wav'.format(chord)
+    })
+    response = save_file(audio)
+    if not response:
+        return {}
+    return {
+        'url': response.url,
+        'duration': audio.duration
+    }
+
+if __name__ == "__main__":
+    pass
